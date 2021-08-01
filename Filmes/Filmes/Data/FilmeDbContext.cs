13502dd1aec9878fd07eb36a90df1dfe3c290194
Phillipe Couto <rpcajr@gmail.com>
2021-08-01 15:02:52 -0300

    Initial Commit

---
+++ Filmes/Filmes/Data/FilmeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Filmes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes.Data
+{
+    public class FilmeDbContext : DbContext
+    {
+        public FilmeDbContext(DbContextOptions<FilmeDbContext> options) : base(options){}
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
---
+++ Filmes/Filmes/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmes.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+    }
+}
